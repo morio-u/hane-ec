9b19d9c050091b380c00c5393ea61802f22b144a
上田森郎 <morioueda1004@gmail.com>
2025-08-25 23:39:58 +0900

    Authentication function implementation in progress

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from app.routers import products
+from app.routers import products, auth, users
 
 app = FastAPI()
 
@@ -8,3 +8,5 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="app/static/shop"), name="static")
 
 app.include_router(products.router)
+app.include_router(auth.router, prefix="/auth", tags=["auth"])
+app.include_router(users.router, prefix="/users", tags=["users"])
