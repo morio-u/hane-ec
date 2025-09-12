420a47de3e5f2d07b98ae2ebedd26715642b46d2
上田森郎 <morioueda1004@gmail.com>
2025-09-13 01:12:43 +0900

    Move signup to auth.py

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,74 +1,9 @@
-from datetime import timedelta
-from fastapi import APIRouter, Depends, HTTPException, Form
-from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi import APIRouter, Depends
 from fastapi.templating import Jinja2Templates
-from fastapi import Request
-from sqlalchemy.orm import Session
-from app.core.jwt import create_access_token
-from app.core.config import settings
-from app.crud.user import get_user_by_email, create_user
-from app.database import get_db
 from app.dependencies.auth import get_current_user
-from app.schemas.user import UserCreate
 
 router = APIRouter()
 templates = Jinja2Templates(directory="app/templates/shop")
-
-@router.get("/signup", response_class=HTMLResponse)
-def read_signup_form(request: Request):
-    return templates.TemplateResponse(
-        "signup.html", {"request": request}
-    )
-
-@router.post("/signup")
-def signup(
-        db: Session = Depends(get_db),
-        # TODO: Move to UserCreate
-        last_name: str = Form(...),
-        first_name: str = Form(...),
-        middle_name: str = Form(None),
-        gender: int = Form(...),
-        phone_number: str = Form(...),
-        email: str = Form(...),
-        password: str = Form(...),
-        confirm_password: str = Form(...),
-        is_send_newsletter: bool = Form(False)
-    ):
-    if password != confirm_password:
-        raise HTTPException(status_code=400, detail="Passwords do not match")
-
-    # Check already registered users
-    user_from_db = get_user_by_email(db, email)
-    if user_from_db:
-        raise HTTPException(status_code=400, detail="Email already exists")
-
-    # New User Registration
-    new_user = create_user(
-        db,
-        last_name,
-        first_name,
-        gender,
-        phone_number,
-        email,
-        password,
-        is_send_newsletter,
-        middle_name,
-    )
-
-    access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": str(new_user.email)}, expires_delta=access_token_expires
-    )
-
-    redirect = RedirectResponse(url="/", status_code=303)
-    redirect.set_cookie(
-        key="access_token",
-        value=access_token,
-        httponly=True,
-        max_age=60 * 60,
-        samesite="lax"
-    )
-    return redirect
 
 @router.get("/me")
 def read_users_me(current_user: dict = Depends(get_current_user)):
