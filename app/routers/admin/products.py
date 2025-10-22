1bae0685e3188c28d8506dc60e1630137141ee0d
上田森郎 <morioueda1004@gmail.com>
2025-10-22 22:22:18 +0900

    Develop product page

--- app/routers/admin/products.py
+++ app/routers/admin/products.py
@@ -1,13 +1,14 @@
-from typing import Optional
+from typing import Optional, Union
 from starlette.templating import _TemplateResponse
 from sqlalchemy.orm import Session
 from fastapi import APIRouter, Depends, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from app.core.database import get_db
 from app.core.exception import RedirectHomeException
-from app.crud.shop.products import get_all_products, get_products_with_quantity
+from app.crud.shop.products import get_all_products, get_product_by_id, get_products_with_quantity, get_skus_by_id
 from app.dependencies.auth import get_current_admin_user
+from app.dependencies.session import get_errors_from_session
 from app.models.admin.admin_user import AdminUser
 import logging
 
@@ -37,4 +38,40 @@ def get_products(
 
     return templates.TemplateResponse(
         "products.html", {"request": request, "products": products_with_qty, "user": user}
+    )
+
+
+@router.get("/{product_id}", response_class=HTMLResponse, response_model=None)
+def get_product_detail(
+    request: Request,
+    product_id: int,
+    db: Session = Depends(get_db),
+    user: Optional[AdminUser] = Depends(get_current_admin_user),
+    errors: Optional[list[str]] = Depends(get_errors_from_session),
+) -> Union[_TemplateResponse, RedirectResponse]:
+    try:
+        # Retrieves a product from the database by its ID.
+        product = get_product_by_id(product_id, db)
+
+        # Retrieves all SKUs associated with a given product ID.
+        skus = get_skus_by_id(product_id, db)
+
+        if product is None or skus is None:
+            # For traceback
+            logger.exception(f"Product not found: {product_id}")
+            return RedirectResponse(url="/products", status_code=303)
+    except Exception as e:
+        # For traceback
+        logger.exception(f"Unexpected error in add_to_cart: {e}")
+        raise RedirectHomeException("Unexpected error")
+
+    return templates.TemplateResponse(
+        "product_detail.html",
+        {
+            "request": request,
+            "product": product,
+            "skus": skus,
+            "user": user,
+            "errors": errors,
+        },
     )
